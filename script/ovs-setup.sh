d05900271757f2036ea6597b304546d37e7e4635
sdshayward <s.scott-hayward@qub.ac.uk>
2015-11-12 14:07:32 +0000

    Updated to OVS 2.4.0

--- script/ovs-setup.sh
+++ script/ovs-setup.sh
@@ -8,16 +8,16 @@ fi
 mkdir -p "/opt/ovs/"
 mkdir -p "/opt/ovs/src"
 
-echo "Downloading OpenvSwitch 2.3.1"
-curl http://openvswitch.org/releases/openvswitch-2.3.1.tar.gz | tar -xzC "/opt/ovs/src"
+echo "Downloading OpenvSwitch 2.4.0"
+curl http://openvswitch.org/releases/openvswitch-2.4.0.tar.gz | tar -xzC "/opt/ovs/src"
 
 echo "Installing OVS"
-cd "/opt/ovs/src/openvswitch-2.3.1"
-./configure --prefix=/opt/ovs/2.3.1 --with-linux=/lib/modules/`uname -r`/build
+cd "/opt/ovs/src/openvswitch-2.4.0"
+./configure --prefix=/opt/ovs/2.4.0 --with-linux=/lib/modules/`uname -r`/build
 make
 make install
 make modules_install
 /sbin/modprobe openvswitch
-cp "/opt/ovs/src/openvswitch-2.3.1/datapath/linux/openvswitch.ko" "/opt/ovs/2.3.1/sbin"
+cp "/opt/ovs/src/openvswitch-2.4.0/datapath/linux/openvswitch.ko" "/opt/ovs/2.4.0/sbin"
 
 echo "Done!!"
